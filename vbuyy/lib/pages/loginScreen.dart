ddd2fc007edc8e0cb5234281500ec7232753c198
param-ninthu <ninthu1999@gmail.com>
2022-11-06 22:51:40 +0530

    login ui

--- vbuyy/lib/pages/loginScreen.dart
+++ vbuyy/lib/pages/loginScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:vbuyy/common/themeHelper.dart';
 
 import 'widgets/headerWidget.dart';
 
@@ -13,6 +14,7 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   double _headerHeight = 250;
+  Key _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,15 +27,74 @@ class _LoginPageState extends State<LoginPage> {
               child: HeaderWidget(_headerHeight, true, Icons.login_rounded),
             ),
             SafeArea(
-                child: Container(
-                    child: Column(
-              children: [
-                Text(
-                  'Hello',
-                  style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
-                )
-              ],
-            )))
+              child: Container(
+                padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                child: Column(
+                  children: [
+                    Text(
+                      'Hello',
+                      style:
+                          TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Signin into your account ',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    SizedBox(height: 30.0),
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          TextField(
+                            decoration: ThemeHelper().textInputDecoration(
+                              'Username',
+                              'Enter your username',
+                            ),
+                          ),
+                          SizedBox(height: 30.0),
+                          TextField(
+                            obscureText: true,
+                            decoration: ThemeHelper().textInputDecoration(
+                              'Password',
+                              'Enter your password',
+                            ),
+                          ),
+                          SizedBox(height: 15.0),
+                          Container(
+                            margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+                            alignment: Alignment.topRight,
+                            child: Text('Forgot your password'),
+                          ),
+                          Container(
+                            decoration:
+                                ThemeHelper().buttonBoxDecoration(context),
+                            child: ElevatedButton(
+                              style: ThemeHelper().buttonStyle(),
+                              onPressed: () {},
+                              child: Padding(
+                                padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
+                                child: Text(
+                                  'Sign In'.toUpperCase(),
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                            child: Text('Don\'t have an account? Create'),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
